1b3432237300e89114ebfe4de550a1b4bf03b55d
LorryGailius <lauris.gailius@gmail.com>
2024-01-07 17:18:55 +0200

    Added ability to recieve enum as string in request

--- ZooLink/Program.cs
+++ ZooLink/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("ZooLink"));
 
 // Add services to the container.
+
+builder.Services.AddControllers()
+    .AddJsonOptions(option =>
+    {
+        option.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
